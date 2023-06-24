599d00444df41b4214c15a716fae0afa48af60d7
yasin <akhmadyasin704@gmail.com>
2023-06-24 07:32:53 +0700

    2B

--- src/tm2.java
+++ src/tm2.java
@@ -7,26 +7,20 @@ public class tm2 {
             int start = 10;
             int end = 4500;
 
-            // Menghasilkan deret Fibonacci hingga mencapai batas end
             List<Integer> deretFibonacci = generateDeretFibonacci(end);
 
-            // Menyaring bilangan Fibonacci genap
             List<Integer> Fibonacci = filterNumbers(deretFibonacci);
 
-            // Menampilkan deret bilangan Fibonacci genap
             System.out.println("Deret bilangan Fibonacci genap:");
             for (int num : Fibonacci) {
                 System.out.print(num + " ");
             }
 
-            // Menghitung jumlah bilangan Fibonacci genap
             int totalSum = TotalSum(Fibonacci);
 
-            // Menampilkan total jumlah bilangan Fibonacci genap
             System.out.println("\nTotal bilangan Fibonacci genap: " + totalSum);
         }
 
-        // Menghasilkan deret Fibonacci hingga mencapai batas n
         public static List<Integer> generateDeretFibonacci(int n) {
             List<Integer> deretFibonacci = new ArrayList<>();
             deretFibonacci.add(0);
@@ -41,7 +35,6 @@ public class tm2 {
             return deretFibonacci;
         }
 
-        // Menyaring bilangan genap dari deret Fibonacci
         public static List<Integer> filterNumbers(List<Integer> deret) {
             ArrayList<Integer> Numbers = new ArrayList<>();
             for (int num : deret) {
@@ -52,7 +45,6 @@ public class tm2 {
             return Numbers;
         }
 
-        // Menghitung jumlah bilangan dalam suatu list
         public static int TotalSum(List<Integer> numbers) {
             int sum = 0;
             for (int num : numbers) {
